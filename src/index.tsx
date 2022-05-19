a39d8c54f97fcb46b9c7c10f7b918000fb3daa81
Ivan Trejo <ivantm11@icloud.com>
2022-05-19 00:06:13 -0500

    Initialize fluentUI icons

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeIcons } from '@fluentui/react/lib/Icons';
 
 import appStore from 'store';
 
 import './index.css';
 import App from './App';
+
+initializeIcons();
 
 const root = ReactDOM.createRoot(
   document.getElementById('vifit') as HTMLElement
