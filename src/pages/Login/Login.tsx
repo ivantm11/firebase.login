a625f14687fdd43b504018cf16eed57d435f0d56
Ivan Trejo <ivantm11@icloud.com>
2022-05-18 22:25:10 -0500

    Add fluentUI to the project. Add Login and register components

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,67 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { TextField } from '@fluentui/react';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+
+import styles from './Login.module.scss';
 
 const Login: FC = () => {
-  return <div>Login page</div>;
+  const [userMail, setUserMail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const updateUserMail = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue = ''
+  ) => {
+    event.preventDefault();
+    setUserMail(newValue);
+  };
+
+  const updateUserPassword = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue = ''
+  ) => {
+    event.preventDefault();
+    setUserPassword(newValue);
+  };
+
+  const someFieldIsEmpty = () => userMail === '' || userPassword === '';
+
+  const logInClicked = () => console.info('Log In');
+
+  return (
+    <div className={styles.Login}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>{`Log In`}</h2>
+        <TextField
+          label="Email"
+          type="email"
+          value={userMail}
+          onChange={updateUserMail}
+          required
+        />
+        <TextField
+          label="Password"
+          value={userPassword}
+          onChange={updateUserPassword}
+          type="password"
+          canRevealPassword
+          revealPasswordAriaLabel="Show password"
+          required
+        />
+        <PrimaryButton
+          text="Log In"
+          onClick={logInClicked}
+          disabled={someFieldIsEmpty()}
+          className={styles.Go}
+        />
+        <p className={styles.message}>
+          {`If you don't have an account, `}
+          <br />
+          <a href="/register"> {`you can create one here.`}</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,7 +1,91 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { TextField } from '@fluentui/react';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+
+import styles from './Register.module.scss';
 
 const Register: FC = () => {
-  return <div>Register Page</div>;
+  const [userMail, setUserMail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userConfirmPassword, setUserConfirmPassword] = useState('');
+
+  const updateUserMail = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue = ''
+  ) => {
+    event.preventDefault();
+    setUserMail(newValue);
+  };
+
+  const updateUserPassword = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue = ''
+  ) => {
+    event.preventDefault();
+    setUserPassword(newValue);
+  };
+
+  const updateUserConfirmPassword = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue = ''
+  ) => {
+    event.preventDefault();
+    setUserConfirmPassword(newValue);
+  };
+
+  const someFieldIsEmpty = () =>
+    userMail === '' || userPassword === '' || userConfirmPassword === '';
+
+  const passwordsAreNotTheSame = () => userPassword !== userConfirmPassword;
+
+  const confirmIsDisabled = () =>
+    someFieldIsEmpty() || passwordsAreNotTheSame();
+
+  const createClicked = () => console.info('Create');
+
+  return (
+    <div className={styles.Register}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>{`Register`}</h2>
+        <TextField
+          label="Email"
+          type="email"
+          value={userMail}
+          onChange={updateUserMail}
+          required
+        />
+        <TextField
+          label="Password"
+          value={userPassword}
+          onChange={updateUserPassword}
+          type="password"
+          canRevealPassword
+          revealPasswordAriaLabel="Show password"
+          required
+        />
+        <TextField
+          label="Confirm password"
+          value={userConfirmPassword}
+          onChange={updateUserConfirmPassword}
+          type="password"
+          canRevealPassword
+          revealPasswordAriaLabel="Show password"
+          required
+        />
+        <PrimaryButton
+          text="Create account"
+          onClick={createClicked}
+          disabled={confirmIsDisabled()}
+          className={styles.Go}
+        />
+        <p className={styles.message}>
+          {`Already have an account?`}
+          <br />
+          <a href="/login"> {`Log in here.`}</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
