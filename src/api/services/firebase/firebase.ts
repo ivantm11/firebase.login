f15f90606e33928605d6082472f544c085640dba
Ivan Trejo <ivantm11@icloud.com>
2022-06-04 21:32:48 -0500

    Add Firebase config file

--- src/api/services/firebase/firebase.ts
+++ src/api/services/firebase/firebase.ts
@@ -5,7 +5,7 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebaseConfig from './firebaseConfig';
+import firebaseConfig from './firebaseConfig.template';
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
